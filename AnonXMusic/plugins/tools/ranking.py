50049e0b3ef4fdce0424f2f1d777f3aa09fc02d9
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-13 09:41:09 +0530

    Update ranking.py
    Signed-off-by: darkmadara09 <136217534+darkmadara09@users.noreply.github.com>

--- AnonXMusic/plugins/tools/ranking.py
+++ AnonXMusic/plugins/tools/ranking.py
@@ -87,14 +87,6 @@ async def today_(_, message):
         sorted_users_data = sorted(users_data, key=lambda x: x[1], reverse=True)[:10]
 
         if sorted_users_data:
-            response = "✦ 📈 ᴛᴏᴅᴀʏ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ\n\n"
-            for idx, (user_id, total_messages) in enumerate(sorted_users_data, start=1):
-                try:
-                    user_name = (await app.get_users(user_id)).first_name
-                except:
-                    user_name = "Unknown"
-                user_info = f"{idx}.   {user_name} ➠ {total_messages}\n"
-                response += user_info
             total_messages_count = sum(user_data['total_messages'] for user_data in today[chat_id].values())
             response = f"✦ 📈 ᴛᴏᴅᴀʏ ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs: {total_messages_count}\n\n"    
             for idx, (user_id, total_messages) in enumerate(sorted_users_data, start=1):
